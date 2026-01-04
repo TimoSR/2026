c0fe87d7b8d87fd1a4b7abee9bd51558c4e0eea9
TimoSR <timothy.s.rasmussen@gmail.com>
2026-01-04 22:03:52 +0100

    project ready

---
+++ HackerRank/HackerRank/Example/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace HackerRank;
+
+public class Calculator : ICalculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    public int Power(int a, int b)
+    {
+        return a ^ b;
+    }
+}
---
+++ HackerRank/Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using HackerRank;
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class CalculatorTests(ITestOutputHelper testOutputHelper)
+{
+    private readonly ICalculator _calculator = new Calculator();
+
+    [Fact]
+    public void AddingTwoNumbers()
+    {
+        var result = _calculator.Add(2, 2);
+        testOutputHelper.WriteLine(result.ToString());
+        Assert.Equal(4, result);
+    }
+    
+    [Fact]
+    public void SubtractingTwoNumbers()
+    {
+        var result = _calculator.Subtract(2, 2);
+        Assert.Equal(0, result);
+    }
+    
+    [Fact]
+    public void MultiplyingTwoNumbers()
+    {
+        var result = _calculator.Multiply(2, 2);
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void DividingTwoNumbers()
+    {
+        var result = _calculator.Divide(2, 2);
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void CantDivideByZero()
+    {
+        Assert.Throws<DivideByZeroException>(() => _calculator.Divide(0, 0));
+    }
+}
