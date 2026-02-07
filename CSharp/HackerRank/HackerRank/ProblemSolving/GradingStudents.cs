543c4df01fb00fbfa8f20939d0cdaf035105c52c
TimoSR <timothy.s.rasmussen@gmail.com>
2026-02-07 09:30:15 +0100

    finished solution

--- CSharp/HackerRank/HackerRank/ProblemSolving/GradingStudents.cs
+++ CSharp/HackerRank/HackerRank/ProblemSolving/GradingStudents.cs
@@ -4,38 +4,33 @@ using System.Collections.Generic;
 public class GradingStudents()
 {
     private static readonly Func<int, bool> IsFailing = grade => grade < 38;
+    private static readonly Func<int, bool> IsNotFailing = grade => !IsFailing(grade);
     private static readonly Func<int, bool> IsMultipleOfFive = grade => grade % 5 == 0;
+    private static readonly Func<int, bool> IsNotMultipleOfFive = grade => !IsMultipleOfFive(grade);
+
+    public static int Add(int a, int b) => a + b;
 
     public static List<int> GradingStudentsMethodWithDelegates(List<int> grades)
     {
-        if (grades == null)
-        {
-            throw new ArgumentNullException(nameof(grades));
-        }
-
         var roundedGrades = new List<int>(grades.Count);
 
         foreach (var grade in grades)
-        {
-            if (IsFailing(grade))
-            {
-                roundedGrades.Add(grade);
-                continue;
-            }
+        {   
+            var result = grade;
 
-            if (!IsMultipleOfFive(grade))
+            if(IsNotFailing(result) && IsNotMultipleOfFive(result))
             {
-                var remainder = grade % 5;
+                var remainder = result % 5;
+
                 var delta = 5 - remainder;
 
-                if (delta <= 2)
+                if (delta < 3)
                 {
-                    roundedGrades.Add(grade + delta);
-                    continue;
+                    result += delta;
                 }
             }
-
-            roundedGrades.Add(grade);
+            
+            roundedGrades.Add(result);
         }
 
         return roundedGrades;
@@ -43,37 +38,30 @@ public class GradingStudents()
 
     public static List<int> GradingStudentsMethodWithLamdas(List<int> grades)
     {
-        if (grades == null)
-        {
-            throw new ArgumentNullException(nameof(grades));
-        }
-
         var isFailing = (int grade) => grade < 38;
+        var isNotFailing = (int grade) => !isFailing(grade);
         var isMultipleOfFive = (int grade) => grade % 5 == 0;
+        var isNotMultipleOfFive = (int grade) => !isMultipleOfFive(grade);
 
         var roundedGrades = new List<int>(grades.Count);
 
         foreach (var grade in grades)
-        {
-            if (isFailing(grade))
-            {
-                roundedGrades.Add(grade);
-                continue;
-            }
+        {   
+            var result = grade;
 
-            if (!isMultipleOfFive(grade))
+            if(isNotFailing(result) && isNotMultipleOfFive(result))
             {
-                var remainder = grade % 5;
+                var remainder = result % 5;
+
                 var delta = 5 - remainder;
 
-                if (delta <= 2)
+                if (delta < 3)
                 {
-                    roundedGrades.Add(grade + delta);
-                    continue;
+                    result += delta;
                 }
             }
-
-            roundedGrades.Add(grade);
+            
+            roundedGrades.Add(result);
         }
 
         return roundedGrades;
