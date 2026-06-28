2b1e0c74c68d00c0d9bb5dea74cdfb5677ec98b8
TimoSR <timothy.s.rasmussen@gmail.com>
2026-06-28 19:58:35 +0200

    works

--- Best_Research/#Done/C++/Units-CppXmake/aztro_physics/xmake.lua
+++ Best_Research/#Done/C++/Units-CppXmake/aztro_physics/xmake.lua
@@ -1,4 +1,4 @@
-set_project("AZTROPhysics")
+set_project("aztro_physics")
 set_version("0.1.0")
 
 set_languages("c++23")
@@ -13,7 +13,7 @@ local warning_flags = function()
     add_cxxflags("-Wall", "-Wextra", "-Wpedantic", {tools = {"clang", "gcc"}})
 end
 
-target("AZTROPhysics")
+target("aztro_physics")
     set_kind("static")
     set_toolchains("clang-cl")
     add_files(path.join(aztro_physics_dir, "Physics.cppm"), {public = true})
@@ -22,11 +22,11 @@ target("AZTROPhysics")
     remove_files(path.join(aztro_physics_dir, "**/tests.cpp"))
     warning_flags()
 
-target("AZTROPhysicsSmoke")
+target("aztro_physicsSmoke")
     set_kind("binary")
     set_default(false)
     set_rundir(os.projectdir())
     set_toolchains("clang-cl")
     add_files(path.join(aztro_physics_dir, "tests/main.cpp"))
-    add_deps("AZTROPhysics")
+    add_deps("aztro_physics")
     warning_flags()
